560b34ac74b3d032d20c5fffacc633d0ad17823a
Yasar icli <yasaricli@gmail.com>
2020-01-29 13:25:51 +0300

    example application added #5

---
+++ example/App.js
@@ -0,0 +1,70 @@
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import Dots from 'react-native-dots-pagination';
+import _ from 'underscore';
+
+class Example extends Component {
+  state = {
+    active0: 0,
+    active1: 1,
+    active2: 2,
+    active3: 3,
+  };
+
+  changePress() {
+    this.setState({
+      active0: _.random(0, 5),
+      active1: _.random(0, 10),
+      active2: _.random(0, 15),
+      active3: _.random(0, 20),
+    });
+  }
+
+  render() {
+    return (
+      <SafeAreaView>
+          <Dots length={5} active={this.state.active0} />
+          <Dots length={10} active={this.state.active1} />
+          <Dots length={15} active={this.state.active2} />
+          <Dots length={20} active={this.state.active3} />
+
+        <View style={Styles.footer}>
+          <TouchableOpacity
+            style={Styles.button}
+            onPress={this.changePress.bind(this)}>
+            <Text style={Styles.buttonText}>CHANGE</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+const Styles = StyleSheet.create({
+  footer: {
+    alignItems: 'center',
+    marginVertical: 15,
+  },
+
+  button: {
+    width: 100,
+    padding: 10,
+    backgroundColor: '#333',
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
+
+export default Example;
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { View, ScrollView, Platform } from "react-native";
 import PropTypes from "prop-types";
 
